9ebb4d8e1a6861ace178460fcae58e886163c23b
baptsynergie <baptiste.gauthier@synergiefamily.com>
2025-04-15 16:51:17 +0200

    finition v1 inte

---
+++ components/PresSection.vue
@@ -0,0 +1,57 @@
+<template>
+  <section class="bg-light-black ">
+    <div class="max-w-[1815px] mx-auto px-6 pb-16 2xl:pb-32 lg:flex lg:gap-8">
+
+      <div class="h-[370px] lg:h-[430px] lg:flex-[1.3]">
+        <img class="h-full w-full object-cover object-top" src="~/assets/images/baptiste.png"
+             alt="Baptiste Gauthier - Développeur FullStack">
+      </div>
+
+      <div class="flex mt-6 lg:flex-[2]">
+        <div class="flex-1">
+          <div class="text-white mb-4 uppercase dm-sans-medium underline">
+            Skills :
+          </div>
+          <ul>
+            <li class="text-white uppercase dm-sans-medium">html/css</li>
+            <li class="text-white uppercase dm-sans-medium">javascript</li>
+            <li class="text-white uppercase dm-sans-medium">typescript</li>
+            <li class="text-white uppercase dm-sans-medium">symfony</li>
+            <li class="text-white uppercase dm-sans-medium">twig component</li>
+            <li class="text-white uppercase dm-sans-medium">nest js</li>
+            <li class="text-white uppercase dm-sans-medium">angular</li>
+            <li class="text-white uppercase dm-sans-medium">tailwind</li>
+            <li class="text-white uppercase dm-sans-medium">astro</li>
+            <li class="text-white uppercase dm-sans-medium">gsap</li>
+            <li class="text-white uppercase dm-sans-medium">figma</li>
+          </ul>
+        </div>
+        <div class="flex-1">
+          <div class="text-white mb-4 uppercase dm-sans-medium underline">
+            My favourite tools :
+          </div>
+          <ul>
+            <li class="text-white uppercase dm-sans-medium">datagrip</li>
+            <li class="text-white uppercase dm-sans-medium">laragon</li>
+            <li class="text-white uppercase dm-sans-medium">git</li>
+            <li class="text-white uppercase dm-sans-medium mb-4">notion</li>
+            <li class="text-white uppercase dm-sans-medium">photoshop</li>
+            <li class="text-white uppercase dm-sans-medium">illustrator</li>
+          </ul>
+        </div>
+      </div>
+
+      <div class="mt-6 space-y-6 lg:flex-[2] lg:content-end">
+        <p class="dm-sans-light uppercase text-white">Danish pie icing brownie shortbread. Chupa chups biscuit cookie croissant gingerbread topping cookie cupcake.
+          Chupa chups cheesecake cotton candy cookie marshmallow biscuit jelly. Cake halvah candy canes gingerbread
+          chocolate.</p>
+        <p class="dm-sans-light uppercase text-white">Sweet sweet roll fruitcake cupcake caramels. Chocolate candy canes cookie tart bear claw. Chocolate bar
+          toffee toffee dragée chocolate cake croissant lemon drops cotton candy. Danish chocolate bar tootsie roll
+          chupa chups danish.</p>
+      </div>
+    </div>
+  </section>
+
+</template>
+<script setup lang="ts">
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -10,7 +10,8 @@
             Gauthier
           </h2>
         </div>
-        <svg class="star w-[36px] md:w-[56px]" width="56" height="56" viewBox="0 0 56 56" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <svg class="star w-[36px] md:w-[56px]" width="56" height="56" viewBox="0 0 56 56" fill="none"
+             xmlns="http://www.w3.org/2000/svg">
           <g clip-path="url(#clip0_49_343)">
             <path d="M0 28H56" stroke="#262626" stroke-width="5" stroke-miterlimit="10"/>
             <path d="M28 0V56" stroke="#262626" stroke-width="5" stroke-miterlimit="10"/>
@@ -35,7 +36,8 @@
                  alt="image pres"/>
           </div>
         </div>
-        <p class="order-1 text-center md:text-left uppercase self-end font-medium dm-sans-medium md:max-w-lg text-light-black">Tiramisu sesame
+        <p class="order-1 text-center md:text-left uppercase self-end font-medium dm-sans-medium md:max-w-lg text-light-black">
+          Tiramisu sesame
           snaps donut chocolate cake muffin jelly-o pudding
           pie. Topping sugar plum croissant candy
           muffin bonbon lemon drops chocolate. Chocolate bar jujubes marzipan ice cream pie dragée donut cheesecake.</p>
@@ -54,11 +56,54 @@
     <Project title="Passer le BAFA" year="2025" from="Synergie Family" content="Passer le BAFA"/>
 
   </section>
+
+  <section class="my-16 px-6 lg:my-32 max-w-6xl mx-auto">
+    <div class="dm-sans-medium text-light-black text-2xl lg:text-3xl uppercase">
+      Pudding cheesecake danish candy candy canes. Jelly-o toffee candy tootsie roll gummi bears macaroon croissant
+      <svg class="mx-2 star inline" width="36" height="36" viewBox="0 0 56 56" fill="none"
+           xmlns="http://www.w3.org/2000/svg">
+        <g clip-path="url(#clip0_49_343)">
+          <path d="M0 28H56" stroke="#262626" stroke-width="5" stroke-miterlimit="10"/>
+          <path d="M28 0V56" stroke="#262626" stroke-width="5" stroke-miterlimit="10"/>
+          <path d="M8.18945 47.8107L47.811 8.18924" stroke="#262626" stroke-width="5" stroke-miterlimit="10"/>
+          <path d="M8.18945 8.18924L47.811 47.8107" stroke="#262626" stroke-width="5" stroke-miterlimit="10"/>
+        </g>
+        <defs>
+          <clipPath id="clip0_49_343">
+            <rect width="56" height="56" fill="white"/>
+          </clipPath>
+        </defs>
+      </svg>
+      <span class="opacity-50">jujubes cupcake. Tart lemon drops marshmallow macaroon tart chocolate jujubes. Soufflé toffee
+      <svg class="mx-2 star inline" width="36" height="36" viewBox="0 0 56 56" fill="none"
+           xmlns="http://www.w3.org/2000/svg">
+        <g clip-path="url(#clip0_49_343)">
+          <path d="M0 28H56" stroke="#262626" stroke-width="5" stroke-miterlimit="10"/>
+          <path d="M28 0V56" stroke="#262626" stroke-width="5" stroke-miterlimit="10"/>
+          <path d="M8.18945 47.8107L47.811 8.18924" stroke="#262626" stroke-width="5" stroke-miterlimit="10"/>
+          <path d="M8.18945 8.18924L47.811 47.8107" stroke="#262626" stroke-width="5" stroke-miterlimit="10"/>
+        </g>
+        <defs>
+          <clipPath id="clip0_49_343">
+            <rect width="56" height="56" fill="white"/>
+          </clipPath>
+        </defs>
+      </svg>
+      gummies cotton candy
+      chocolate cake caramels danish ice cream candy.</span>
+    </div>
+  </section>
+
+  <InfiniteSlider />
+  <PresSection />
+  <ContactSection />
 </template>
 <script setup lang="ts">
 
 import {gsap} from "gsap";
 import {onMounted} from "vue";
+import PresSection from "~/components/PresSection.vue";
+import ContactSection from "~/components/ContactSection.vue";
 
 onMounted(() => {
   gsap.to('.star', {rotation: 360, duration: 2, repeat: -1, transformOrigin: 'center', ease: 'none'});
