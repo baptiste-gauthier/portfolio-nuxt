27d41cadff9bb1a112f43bd58146157797b89727
baptsynergie <baptiste.gauthier@synergiefamily.com>
2025-05-07 14:02:33 +0200

    modif taille font

--- components/ContactSection.vue
+++ components/ContactSection.vue
@@ -3,8 +3,8 @@
     <div class="max-w-[1815px] mx-auto px-6">
 
       <div class="lg:flex items-center gap-6 overflow-hidden">
-        <h2 data-splitting class="overflow-reveal font-kenoky text-7xl lg:text-9xl uppercase text-white">Contactez</h2>
-        <h3 data-splitting class="overflow-reveal dm-sans-bold text-7xl lg:text-9xl uppercase text-white lg:mt-4">Moi</h3>
+        <h2 data-splitting class="overflow-reveal font-kenoky text-5xl md:text-7xl lg:text-9xl uppercase text-white">Contactez</h2>
+        <h3 data-splitting class="overflow-reveal dm-sans-bold text-5xl md:text-7xl lg:text-9xl uppercase text-white lg:mt-4">Moi</h3>
       </div>
 
       <div class="space-y-2 mt-8">
